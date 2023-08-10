fd23f0d3309c47f17fd4673ea2b67388559bcaab
Rufus Jones <rufuscodes@gmail.com>
2023-08-09 17:38:32 -0700

    add: mongodb from atlas imported into backend, creds in .env . Created User model

--- backend-hella-recipes/src/index.js
+++ backend-hella-recipes/src/index.js
@@ -1,3 +1,6 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import express from "express";
 import cors from "cors"
 import mongoose from "mongoose"
@@ -6,5 +9,23 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+mongoose.connect(process.env.DB_CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('Could not connect to MongoDB', err));
+
+
+
+
+
+
+
+
+
+
+
 
 app.listen(31337, () => console.log("Server running on PORT 31337"));
