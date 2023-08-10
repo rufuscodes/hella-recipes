7d07df4abf9493427380dc44ebc10fab9d76df23
Rufus Jones <rufuscodes@gmail.com>
2023-08-10 13:36:29 -0700

    create: savedRecipes, savedRecipes/ids/ GET routes, plus PUT route for /recipes

--- backend-hella-recipes/src/routes/recipes.js
+++ backend-hella-recipes/src/routes/recipes.js
@@ -1,5 +1,6 @@
 import express from "express"
 import { RecipeModel } from "../models/Recipes.js";
+import { UserModel } from "../models/Users.js";
 
 const router = express.Router();
 
@@ -20,6 +21,42 @@ router.post("/", async (req, res) => {
         res.json(response)
     } catch (error) {
         res.status(500).json(error);
+    }
+})
+
+
+router.put("/", async (req, res) => {
+    try {
+        const recipe = await RecipeModel.findById(req.body.recipeId);
+        const user = await UserModel.findById(req.body.userId);
+        user.savedRecipes.push(recipe);
+        await user.save();
+
+        res.json({ savedRecipes: user.savedRecipes });
+    } catch (error) {
+        res.status(500).json(error);
+    }
+})
+
+router.get("/savedRecipes/ids/", async (req, res) => {
+    try {
+        const user = await UserModel.findById(req.body.userId);
+        res.json({ savedRecipes: user?.savedRecipes })
+
+    } catch (error) {
+        res.json(error)
+    }
+})
+
+router.get("/savedRecipes", async (req, res) => {
+    try {
+        const user = await UserModel.findById(req.body.userId);
+        const savedRecipes = await RecipesModel.find({
+            _id: { $in: user.savedRecipes },
+        });
+        res.json({ savedRecipes })
+    } catch (error) {
+        res.json(error)
     }
 })
 
