91677c09bd208dcd23411866ee2839478f47ed64
Rufus Jones <rufuscodes@gmail.com>
2023-08-11 17:56:14 -0700

    create: Recipe, RecipeIngredients and Ingredients model. Add: models in admin.py to display in the django administration panel

--- recipes_app/admin.py
+++ recipes_app/admin.py
@@ -1,1 +1,7 @@
 from django.contrib import admin
+
+from . import models
+
+admin.site.register(models.Recipe)
+admin.site.register(models.Ingredient)
+admin.site.register(models.RecipeIngredient)
--- recipes_app/models.py
+++ recipes_app/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Recipe(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    author = models.ForeignKey(User, related_name='recipes', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='recipes/', null=True, blank=True)
+    favorited_by = models.ManyToManyField(User, related_name='favorited_recipes', blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=500)
+    image_url = models.URLField(blank=True, null=True) 
+
+    def __str__(self):
+        return self.name
+
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    quantity = models.DecimalField(max_digits=5, decimal_places=2)
+    measurement = models.CharField(max_length=50)  # grams, cups, tablespoons
+
+    def __str__(self):
+        return f"{self.quantity} {self.measurement} of {self.ingredient.name} for {self.recipe.title}"
