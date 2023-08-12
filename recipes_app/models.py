c777aeede178eec02f8b58ab1209be005123ca36
Rufus Jones <rufuscodes@gmail.com>
2023-08-12 13:11:29 -0700

    add: create recipes url with LoginRequired Mixins

--- recipes_app/models.py
+++ recipes_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class Recipe(models.Model):
     title = models.CharField(max_length=255)
@@ -9,6 +10,10 @@ class Recipe(models.Model):
     favorited_by = models.ManyToManyField(User, related_name='favorited_recipes', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    
+    def get_absolute_url(self):
+        return reverse("recipes-detail", kwargs={"pk": self.pk})
+    
 
     def __str__(self):
         return self.title
@@ -29,3 +34,5 @@ class RecipeIngredient(models.Model):
 
     def __str__(self):
         return f"{self.quantity} {self.measurement} of {self.ingredient.name} for {self.recipe.title}"
+
+
--- recipes_app/urls.py
+++ recipes_app/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 from . import views
 
 'app/modle_viewtype'
-'reecipes/recipe_detail.html'
+'recipes_app/recipe_detail.html'
 
 urlpatterns = [
     path('', views.RecipeListView.as_view(), name="recipes-home"),
     path('recipes/<int:pk>/', views.RecipeDetailView.as_view(), name="recipes-detail"),
+    path('recipe/create/', views.RecipeCreateView.as_view(), name="recipes-create"),
 
     path('about/', views.about, name="recipes-about"),
 
